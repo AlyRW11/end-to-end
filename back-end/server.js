a8416497e2493ccd5b0b210f1541690b255fd58f
alyr.wayne <alegab89@hotmail.co.uk>
2018-01-29 20:44:52 -0700

    adding reduce for makes

--- back-end/server.js
+++ back-end/server.js
@@ -74,6 +74,87 @@ const getCarsCollection = async () => {
     return cars
 }
 
+const findCarWithMaxMileage = (prevCar, currCar) => {
+    return prevCar.mileage > currCar.mileage ? prevCar : currCar
+}
+
+server.route({
+    method: 'GET',
+    path:'/cars/mileage/max', 
+    handler: async (request, h) => {
+        const carsCollection = await getCarsCollection()
+        const cars = await carsCollection.find()
+
+        if (cars.length === 0)
+            return { car: null }
+
+        const carWithMaxMileage = cars
+            .reduce(findCarWithMaxMileage)
+
+        return { car: carWithMaxMileage }
+    },
+    config: {
+        cors: {
+            origin: ['*'],
+            additionalHeaders: ['cache-control', 'x-requested-with']
+        }
+    }
+})
+
+const findCarWithMinMileage = (prevCar, currCar) => {
+    return prevCar.mileage < currCar.mileage ? prevCar : currCar
+}
+
+server.route({
+    method: 'GET',
+    path:'/cars/mileage/min', 
+    handler: async (request, h) => {
+        const carsCollection = await getCarsCollection()
+        const cars = await carsCollection.find()
+
+        if (cars.length === 0)
+            return { car: null }
+
+        const carWithMinMileage = cars
+            .reduce(findCarWithMinMileage)
+
+        return { car: carWithMinMileage }
+    },
+    config: {
+        cors: {
+            origin: ['*'],
+            additionalHeaders: ['cache-control', 'x-requested-with']
+        }
+    }
+})
+
+const findCarMake = (prevCar, currCar) => {
+    return (prevCar.make === undefined ? prevCar : prevCar.make) + ', ' + currCar.make
+}
+
+server.route({
+    method: 'GET',
+    path:'/cars/make/all', 
+    handler: async (request, h) => {
+        const carsCollection = await getCarsCollection()
+        const cars = await carsCollection.find()
+
+        if (cars.length === 0)
+            return { car: null }
+
+        const carMake = cars
+            .reduce(findCarMake)
+
+        return { makes: carMake }
+    },
+    config: {
+        cors: {
+            origin: ['*'],
+            additionalHeaders: ['cache-control', 'x-requested-with']
+        }
+    }
+})
+
 server.route({
     method: 'POST',
     path: '/cars',
